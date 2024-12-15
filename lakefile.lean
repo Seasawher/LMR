17dbcf6ded156e35001655407b9ab3c9ceaad0f3
Seasawher <tellagoodstory@icloud.com>
2024-12-15 22:02:43 +0900

    lakefile 更新

--- lakefile.lean
+++ lakefile.lean
@@ -4,9 +4,10 @@ open Lake DSL
 package "LMR" where
   -- add package configuration options here
 
+@[default_target]
 lean_lib «LMR» where
   -- add library configuration options here
+  globs := #[.submodules `LMR]
 
-@[default_target]
 lean_exe "lmr" where
   root := `Main
